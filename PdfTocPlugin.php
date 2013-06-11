a155027d1a7ddb41cc551a592ea221f426857dfb
Julien Sicot <julien.sicot@gmail.com>
2013-06-11 12:19:34 +0200

    Upgraded for omeka 2.0 based on PDF Text Plugin

--- PdfTocPlugin.php
+++ PdfTocPlugin.php
@@ -1,215 +1,177 @@
 <?php
 /**
- * Make PDF ToC available in Omeka.
+ * PDF ToC
+ *
+ * Adapted from PDF Text plugin
  * 
- * derivated from PdfSearch plugin.
+ * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
-class PdfTocPlugin extends Omeka_Plugin_Abstract
+
+/**
+ * The PDF Text plugin.
+ * 
+ * @package Omeka\Plugins\PdfToc
+ */
+class PdfTocPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    const ELEMENT_SET_NAME = 'PDF Table Of Content';
+    const ELEMENT_SET_NAME = 'PDF Table of Contents';
     const ELEMENT_NAME = 'Text';
-    
-    protected $_hooks = array('install', 'uninstall', 'after_save_item', 
-                              'after_delete_file', 'config', 'config_form');
-   
-    protected $_pdfMimeTypes = array('application/pdf', 'application/x-pdf', 
-                                     'application/acrobat', 'text/x-pdf', 
-                                     'text/pdf', 'applications/vnd.pdf');
-    
+
+    protected $_hooks = array(
+        'install',
+        'uninstall',
+        'config_form',
+        'config',
+        'before_save_file',
+    );
+
+    protected $_pdfMimeTypes = array(
+        'application/pdf',
+        'application/x-pdf',
+        'application/acrobat',
+        'text/x-pdf',
+        'text/pdf',
+        'applications/vnd.pdf',
+    );
+
     /**
      * Install the plugin.
      */
     public function hookInstall()
     {
-        // Don't install if a PDF Search element set already exists.
-        if ($this->_db->getTable('ElementSet')->findByName(self::ELEMENT_SET_NAME)) {
-            throw new Exception('An element set by the name "' . self::ELEMENT_SET_NAME . '" already exists. You must delete that element set to install this plugin.');
-        }
-        
         // Don't install if the pdftk command doesn't exist.
-        $output = (int) shell_exec('hash pdftk 2>&- || echo -1');
-        if (-1 == $output) {
-            throw new Exception('The pdftk command-line utility is not installed. pdftotext must be installed to install this plugin.');
+        // See: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+        if ((int) shell_exec('hash pdftk 2>&- || echo -1')) {
+            throw new Omeka_Plugin_Installer_Exception(__('The pdftk command-line utility ' 
+            . 'is not installed. pdftk must be installed to install this plugin.'));
         }
-        
-        // Insert the element set.
-        $elementSetMetadata = array('name' => self::ELEMENT_SET_NAME, 
-                                    'description' => 'This element set enables storing TOC od PDF files.');
-        $elements = array(array('name' => self::ELEMENT_NAME, 
-                                'description' => 'TOC extracted from PDF files belonging to this item. One line per element, looking like page|title'));
-        insert_element_set($elementSetMetadata, $elements);
+        // Don't install if a PDF element set already exists.
+        if ($this->_db->getTable('ElementSet')->findByName(self::ELEMENT_SET_NAME)) {
+            throw new Omeka_Plugin_Installer_Exception(__('An element set by the name "%s" already ' 
+            . 'exists. You must delete that element set to install this plugin.', self::ELEMENT_SET_NAME));
+        }
+        insert_element_set(
+            array('name' => self::ELEMENT_SET_NAME, 'record_type' => 'File'),
+            array(array('name' => self::ELEMENT_NAME))
+        );
     }
-    
+
     /**
-     * Uninstall the plugin.
+     * Uninstall the plugin
      */
     public function hookUninstall()
     {
-        // Delete the PDF Search element set.
+        // Delete the PDF element set.
         $this->_db->getTable('ElementSet')->findByName(self::ELEMENT_SET_NAME)->delete();
     }
-    
-    /**
-     * Refresh PDF texts to account for an item save.
-     */
-    public function hookAfterSaveItem($item)
-    {
-        # print $_POST['force-toc-update'];
-        $this->saveItemPdfToc($item);
-    }
-    
-    /**
-     * Refresh PDF texts to account for a file delete.
-     */
-    public function hookAfterDeleteFile($file)
-    {
-        $item = $file->getItem();
-        $this->saveItemPdfToc($item);
-    }
 
-		public function hookConfigForm()
-		{
-	?>
-			<div class="field">
-				<label for="save_pdf_tocs">Traiter tous les fichiers PDF</label>
-				<div class="inputs">
-					<?php echo __v()->formCheckbox('extract_pdf_tocs'); ?>
-				</div>
-				<p class="explanation">
-					Ce plugin fait normalement la mise à jour des tables de matières à chaque mise à jour de document. Pour une raison ou pour une autre, il peut etre nécessaire de tout remettre à zéro. Pour cela cocher la base ci-dessus et "Sauvegarder les changements"
-				</p>
-			</div> <!-- #field -->
-	<?php
-		}
 
     /**
-    * Process the plugin config form.
-    */
+     * Display the config form.
+     */
+    public function hookConfigForm()
+    {
+        echo get_view()->partial(
+            'plugins/pdf-toc-config-form.php', 
+            array('valid_storage_adapter' => $this->isValidStorageAdapter())
+        );
+    }
+
+    /**
+     * Handle the config form.
+     */
     public function hookConfig()
-		{
-			// Run the text extraction process if directed to do so.
-			if ($_POST['extract_pdf_tocs']) {
-				ProcessDispatcher::startProcess('PdfTocProcess');
-			}
-		}
+    {
+        // Run the text extraction process if directed to do so.
+        if ($_POST['pdf_toc_process'] && $this->isValidStorageAdapter()) {
+            Zend_Registry::get('bootstrap')->getResource('jobs')
+                ->sendLongRunning('PdfTocProcess');
+        }
+    }
 
     /**
-     * Extract TOC from PDF file, and stores it into the specific field of the parent item
+     * Add the PDF text to the file record.
+     * 
+     * This has a secondary effect of including the text in the search index.
      */
-    
-    public function saveItemPdfToc(Item $item)
+    public function hookBeforeSaveFile($args)
     {
-        $element = $item->getElementByNameAndSetName(self::ELEMENT_NAME, self::ELEMENT_SET_NAME);
-        $recordTypeId = $this->_db->getTable('RecordType')->findIdFromName('item');
-
-        // Iterate the files.
-        foreach ($item->Files as $file) {
-            
-            // Ignore all other file types.
-            if (!in_array($file->mime_browser, $this->_pdfMimeTypes)) {
-                continue;
-            }
-            
-            // Extract the text.
-            $path = escapeshellarg(FILES_DIR . '/' . $file->archive_filename);
-            $cmd = "pdftk $path dump_data";
-            $dump_data = shell_exec($cmd);
-            
-            /* Starging there, we've extracted the dump_data, which looks like :
-                InfoKey: Creator
-                InfoValue: I.C.S. v1.2.7.1 Copyright &#169;  2009-2011 ISAKO
-                InfoKey: Producer
-                InfoValue: iTextSharp 5.0.5 (c) 1T3XT BVBA
-                InfoKey: ModDate
-                InfoValue: D:20111109130842+01'00'
-                InfoKey: CreationDate
-                InfoValue: D:20110722161049+02'00'
-                PdfID0: 6c67c63a772113a953554635eb161a
-                PdfID1: c828f9cd1b17e34aa3a0254447f13a97
-                NumberOfPages: 44
-                BookmarkTitle: LISTE PAR ORDRE ALPHAB&#201;TIQUE DES HOMMES DE SANG
-                BookmarkLevel: 1
-                BookmarkPageNumber: 3
-                BookmarkTitle: A
-                BookmarkLevel: 2
-                BookmarkPageNumber: 3
-            
-            We're interested by things starting at BookmarkTitle. we need to extract these to create something like this:
-            Level|Title|PageNumber
-            */
-            $item->deleteElementTextsByElementId(array($element->id));
-            // We remove anything before the first bookmark
+        // Extract table of contents only on file insert.
+        if (!$args['insert']) {
+            return;
+        }
+        $file = $args['record'];
+        // Ignore non-PDF files.
+        if (!in_array($file->mime_type, $this->_pdfMimeTypes)) {
+            return;
+        }
+        // Add the PDF toc to the file record.
+        $element = $file->getElement(self::ELEMENT_SET_NAME, self::ELEMENT_NAME);
+        $dump_data = $this->pdfToc($file->getPath());
+        // pdftoc must return a string to be saved to the element_texts table.
+        if (is_string($dump_data)) {
+        
+        //preprare ToC
             $dump_data = preg_replace("/^.*(Bookmark.*)$/isU", "$1", $dump_data);
             $dump_data_array = preg_split("/\n/", $dump_data);
-
-            $textToc = "";
+            $dump_data_array = preg_split("/\n/", $dump_data);
+            
+            $toc = "";
             for ($i = 0; $i <= sizeof($dump_data_array); $i+=3)
             {
-                $bm_title   = str_replace("BookmarkTitle: ", "", $dump_data_array[$i]);
-                $bm_level   = str_replace("BookmarkLevel: ", "", $dump_data_array[$i+1]);
-                $bm_page    = str_replace("BookmarkPageNumber: ", "", $dump_data_array[$i+2]);
-                if ($textToc != "")
+                $bm_title = str_replace("BookmarkTitle: ", "", $dump_data_array[$i]);
+                $bm_level = str_replace("BookmarkLevel: ", "", $dump_data_array[$i+1]);
+                $bm_page = str_replace("BookmarkPageNumber: ", "", $dump_data_array[$i+2]);
+                if ($toc != "")
                 {
-                    $textToc .= "\n";
+                    $toc .= "\n";
                 }
                 if ( ($bm_level != "") and ($bm_title != "") and ($bm_page != "") )
                 {
-                    $textToc .= $bm_level."|".$bm_title."|".$bm_page;
+                    $toc .= $bm_level."|".$bm_title."|".$bm_page;
                 }
             }
-            // Save the TOC;
-            $tocRecord = new ElementText;
-            $tocRecord->record_id = $item->id;
-            $tocRecord->element_id = $element->id;
-            $tocRecord->record_type_id = $recordTypeId;
-            $tocRecord->text = $textToc;
-            $tocRecord->html = false;
-            $tocRecord->save();
+            
+            $file->addTextForElement($element, $toc);
         }
-    }    
-    
-    
-    /* We had a checkbox, allowing user to force the update of the TOC.
-    */
-    public static function forceUpdateToc($html, $inputNameStem, $value)
-    {
-        ob_start();
-?>
-        <textarea name="<?php echo $inputNameStem; ?>[text]" class="textinput" rows="15" cols="50"><?php echo $value; ?></textarea>
-        <div style='font-weight:bold; padding-bottom:10px; border-bottom:2px solid #EAE9DB; margin-bottom:10px;'>
-            <input id='force-toc-update' name='force-toc-update' type='checkbox' style='padding:0px; margin:0px;'/>&nbsp;Forcer la mise à jour de la table des matières
-        </div>
-        
-<?php
-        return ob_get_clean();
     }
-    
-    /**
-     * Disable the PDF Search Text form element.
-     * 
-     * There are no circumstances where editing PDF Search Text is needed since 
-     * the plugin overwrites any form submitted data. Form elements with the 
-     * disabled attribute will not be submitted.
-     */
-    public static function disablePdfSearchText($html, $inputNameStem, $value)
-    {
-        ob_start();
-?>
-<textarea name="<?php echo $inputNameStem; ?>[text]" class="textinput" rows="15" cols="50" disabled><?php echo $value; ?></textarea>
-<?php
-        return ob_get_clean();
-    }
-		
-		// Get the ID of the "PDF Toc:Text" element
-		public function getPdfTocElementId()
-		{
-			$item = new Item;
-			return $item->getElementByNameAndSetName(self::ELEMENT_NAME,
-																							 self::ELEMENT_SET_NAME)->id;
-		}
 
-		public function getItemRecordTypeId()
-		{
-			return get_db()->getTable('RecordType')->findIdFromName('item');
-		}
+    /**
+     * Extract the table of contents from a PDF file.
+     * 
+     * @param string $path
+     * @return string
+     */
+    public function pdfToc($path)
+    {
+        $path = escapeshellarg($path);
+        return shell_exec("pdftk $path dump_data");
+    }
+
+    /**
+     * Determine if the plugin supports the storage adapter.
+     * 
+     * 
+     * @return bool
+     */
+    public function isValidStorageAdapter()
+    {
+        $storageAdapter = Zend_Registry::get('bootstrap')
+            ->getResource('storage')->getAdapter();
+        if (!($storageAdapter instanceof Omeka_Storage_Adapter_Filesystem)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get the PDF MIME types.
+     * 
+     * @return array
+     */
+    public function getPdfMimeTypes()
+    {
+        return $this->_pdfMimeTypes;
+    }
 }
--- README.md
+++ README.md
@@ -1,12 +1,93 @@
-Plugin-PdfToc
-=============
+PDF TOC (plugin for Omeka)
+=============================
 
-Omeka plugin to extract TOC from PDF files, and show it on public page. This plugin needs pdftk to extract OCR.
 
-Usage
-=====
-After installation, this plugin can be used by calling the function PdfTocPublicShow() which returns the HTML of the TOC, with links with anchor : items/show/[ID]?page=XX#lire-doc . This link can be modified in plugin.php
+Summary
+-----------
+
+Omeka plugin to extract TOC from PDF files, and show it on public page.
+
+See demo of the in [1886, digital library of university Bordeaux 3 (France)](http://1886.u-bordeaux3.fr/items/show/3953).
+
+
+Installation
+------------
+- This plugin needs pdftk command-line tool on your server
+
+```
+    sudo apt-get install pdftk
+```
+
+- Upload the PDF TOC plugin folder into your plugins folder on the server;
+- you can install the plugin via github
+
+```
+    cd omeka/plugins
+    
+    git clone git@github.com:symac/Plugin-PdfToc.git "PdfToc"
+    
+```
+
+- Activate it from the admin → Settings → Plugins page
+- Click the Configure link to process or not existing PDF files.
+
+
+Using the PDF TOC Plugin
+---------------------------
+
+- Create an item
+- Add PDF file(s) to this item
+- Save Item
+- To locate extracted table of content, select the item to which the PDF is attached. Select File from the Item navigation. Click on the name of the file. 
+
+
+Optional plugins
+----------------
+
+- [BookReader](https://github.com/jsicot/BookReader) : This plugin adds Internet Archive BookReader into Omeka.
+
+See demo of the in [Bibliothèque numérique de l'université Rennes 2 (France)](http://bibnum.univ-rennes2.fr/items/show/572).
+
+
+
+Troubleshooting
+---------------
+
+See online [PDF TOC issues](https://github.com/symac/Plugin-PdfToc/issues).
+
 
 License
-=======
-This plugin is based on PDF Search plugin developped by CHNM ( http://omeka.org/add-ons/plugins/pdf-search/ )
+-------
+
+This plugin is published under [GNU/GPL].
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+Contact
+-------
+
+* Syvain Machefert, Université Bordeaux 3 (see [symac](https://github.com/symac))
+
+
+Copyright
+---------
+
+The source code of [Internet Archive BookReader] is licensed under AGPL v3, as
+described in the LICENSE file.
+
+* Copyright Internet Archive, 2008-2009
+
+
